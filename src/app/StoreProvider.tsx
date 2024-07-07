04a3271d72adc0d9a5e3993f916eac495bc31b62
KienBQ <kaybi9x@gmail.com>
2024-07-07 23:56:12 +0700

    update comp timeline2

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -3,7 +3,6 @@
 import { store } from "@/lib/store";
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
-import { appWithTranslation } from "next-i18next";
 
 const StoreProvider = ({ children }: { children: ReactNode }) => {
   return <Provider store={store}>{children}</Provider>;
